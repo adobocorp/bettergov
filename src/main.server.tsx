ab07888955600ecb2e91a1c9935bfc7ac97ea098
Nicu Listana <niculistana@gmail.com>
2025-10-08 23:54:48 -0700

    feat(ssr): use static router component

--- src/main.server.tsx
+++ src/main.server.tsx
@@ -1,34 +1,38 @@
 import React from 'react';
-import {
-  createStaticRouter,
-  createStaticHandler,
-  StaticHandlerContext,
-  StaticRouterProvider,
-} from 'react-router-dom/server';
-import createFetchRequest from './lib/request.ts';
 import ReactDOMServer from 'react-dom/server';
-import { routes } from './routes.tsx';
 import { HelmetProvider } from 'react-helmet-async';
+import { StaticRouter } from 'react-router-dom/server';
+import { Route, Routes } from 'react-router-dom';
+import NotFound from './pages/NotFound.tsx';
+import Home from './pages/Home.tsx';
+// import Navbar from './components/layout/Navbar';
+// import Ticker from './components/ui/Ticker';
+// import ScrollToTop from './components/ui/ScrollToTop';
+import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar.tsx';
+import { Request } from 'express';
 const helmetContext = {};
-const { query, dataRoutes } = createStaticHandler(routes);
 
-export async function render(req, res) {
-  const fetchRequest = createFetchRequest(req, res);
-  const context = (await query(fetchRequest)) as StaticHandlerContext;
+export async function render(req: Request) {
+  console.log(req.url);
 
-  // If we got a redirect response, short circuit and let our Express server
-  // handle that directly
-  if (context instanceof Response) {
-    throw context;
-  }
-
-  const router = createStaticRouter(dataRoutes, context);
   const html = ReactDOMServer.renderToString(
     <React.StrictMode>
       <HelmetProvider context={helmetContext}>
-        <StaticRouterProvider router={router} context={context} />
+        <StaticRouter location={req.originalUrl}>
+          {/* The rest of your app goes here */}
+          <div className='min-h-screen flex flex-col'>
+            <Navbar />
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+            <Footer />
+          </div>
+        </StaticRouter>
       </HelmetProvider>
     </React.StrictMode>
   );
+
   return { html, helmetContext };
 }
